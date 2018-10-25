051a2c646f3c34f6bd1e9b6c184ce0af50887cfa
PavelSteel <misskiisteel@gmail.com>
2018-10-26 00:40:28 +0300

    загрузил файлы чатика в репозиторий

---
+++ server/AuthService.java
@@ -0,0 +1,37 @@
+package ru.javachat.core.server;
+
+import java.sql.*;
+
+public class AuthService {
+    private Connection connection;
+    private Statement stmt;
+    private PreparedStatement psFindNick;
+
+    public void connect() throws ClassNotFoundException, SQLException{
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:main.db");
+        stmt = connection.createStatement();
+
+    }
+
+    public String getNickByLoginAndPass(String login, String pass){
+        try {
+            ResultSet rs = stmt.executeQuery("SELECT nick FROM users WHERE login = '" + login + "' AND password = '" + pass + "';");
+            while(rs.next()){
+                return rs.getString("nick");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void disconnect(){
+        try {
+            stmt.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
